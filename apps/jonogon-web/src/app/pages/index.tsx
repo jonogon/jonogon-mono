0a6a078de44bfe83aae7731033f19f59ef43f7e2
Shadman Sakib <shadman@felicity.digital>
2024-08-11 19:11:49 +0600

    Add shadcn

--- apps/jonogon-web/src/app/pages/index.tsx
+++ apps/jonogon-web/src/app/pages/index.tsx
@@ -1,25 +1,39 @@
-import {useAuthState, useTokenManager} from '../auth/token-manager.tsx';
-import {trpc} from '../trpc/index.mjs';
-import {useEffect} from 'react';
+import { useAuthState, useTokenManager } from '../auth/token-manager.tsx';
+import { trpc } from '../trpc/index.mjs';
+import { useEffect } from 'react';
+import '@/styles/globals.css';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card.tsx';
 
 export default function Index() {
-    const {get} = useTokenManager();
-    const authState = useAuthState();
+	const { get } = useTokenManager();
+	const authState = useAuthState();
 
-    const {data} = trpc.users.getSelf.useQuery(undefined, {});
+	const { data } = trpc.users.getSelf.useQuery(undefined, {});
 
-    useEffect(() => {
-        console.log(data);
-    }, [data]);
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
 
-    return (
-        <div>
-            <div>Homepage authState</div>
-            <div>
-                <button onClick={() => get({forceRefresh: true})}>
-                    refresh token
-                </button>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div>Homepage authState</div>
+			<div>
+				<button onClick={() => get({ forceRefresh: true })}>
+					refresh token
+				</button>
+			</div>
+			<Card>
+				<CardHeader>
+					<CardTitle>Card Title</CardTitle>
+					<CardDescription>Card Description</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<p>Card Content</p>
+				</CardContent>
+				<CardFooter>
+					<p>Card Footer</p>
+				</CardFooter>
+			</Card>
+		</div>
+	);
 }
