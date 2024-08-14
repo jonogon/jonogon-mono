c607072516c33a1e978980a1b6b5d90500d694a3
imas234 <sikdershadmansami@gmail.com>
2024-08-15 01:20:30 +0600

    Refactor out search and fix submit petition logic

--- apps/jonogon-web/src/app/components/custom/Navigation.tsx
+++ apps/jonogon-web/src/app/components/custom/Navigation.tsx
@@ -14,12 +14,50 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
+import { useAuthState } from '@/app/auth/token-manager';
 
-const Navigation = observer(() => {
+const Search = observer(() => {
     const store = useStore();
     const setStore = useStoreSetter();
     const [openSearch, setOpenSearch] = useState(false);
     const inputRef = useRef<HTMLInputElement>(null);
+
+    return (
+        <div className="flex items-center">
+            <Button
+                variant="link"
+                size="icon"
+                onClick={() => {
+                    setOpenSearch(true);
+                    inputRef.current?.focus();
+                }}>
+                <FiSearch size={18} />
+            </Button>
+            <input
+                ref={inputRef}
+                type="text"
+                onBlur={() =>
+                    !store.filters.search && setOpenSearch(false)
+                }
+                onChange={(e) =>
+                    setStore(
+                        (store) =>
+                            (store.filters.search = e.target.value),
+                    )
+                }
+                value={store.filters.search}
+                placeholder='Search "Petitions"'
+                className={cn(
+                    'transition-all duration-300 w-0 pl-0 h-8 border-none outline-none rounded-md overflow-hidden',
+                    {'w-44': openSearch},
+                )}
+            />
+        </div>
+    );
+})
+
+const Navigation = observer(() => {
+    const authState = useAuthState();
     const [location, navigate] = useLocation();
     const {mutateAsync: createPetition, isLoading} =
         trpc.petitions.create.useMutation();
@@ -37,49 +75,34 @@ const Navigation = observer(() => {
             <div className="container h-16 flex items-center justify-between">
                 <a href="/" className="flex items-center gap-2">
                     <img src="/images/icon.svg" alt="logo" className="w-7" />
-                    <h2 className="text-3xl font-medium">jonogon</h2>
+                    <h2 className="text-2xl font-medium">jonogon</h2>
                 </a>
                 <div className="flex gap items-center">
-                    <div className="flex items-center">
-                        <Button
-                            variant="link"
-                            size="icon"
-                            onClick={() => {
-                                setOpenSearch(true);
-                                inputRef.current?.focus();
-                            }}>
-                            <FiSearch size={18} />
-                        </Button>
-                        <input
-                            ref={inputRef}
-                            type="text"
-                            onBlur={() =>
-                                !store.filters.search && setOpenSearch(false)
-                            }
-                            onChange={(e) =>
-                                setStore(
-                                    (store) =>
-                                        (store.filters.search = e.target.value),
-                                )
-                            }
-                            value={store.filters.search}
-                            placeholder='Search "Petitions"'
-                            className={cn(
-                                'transition-all duration-300 w-0 pl-0 h-8 border-none outline-none rounded-md overflow-hidden',
-                                {'w-44': openSearch},
-                            )}
-                        />
-                    </div>
+                    <Search />
                     <Button
                         variant="link"
-                        onClick={() =>
-                            location !== '/'
-                                ? navigate('/')
-                                : handlePetitionCreate()
-                        }>
-                        {location !== '/'
-                            ? 'Browse petitions'
-                            : 'Start a petition'}
+                        onClick={() => {
+                            if (!authState) {
+                                navigate('/login');
+                                return;
+                            }
+
+                            if (location !== '/') {
+                                navigate('/');
+                                return;
+                            }
+                            else {
+                                handlePetitionCreate()
+                                return;
+                            }    
+                        }}>
+                        {
+                            !authState
+                                ? 'Login to submit petition'
+                                :location !== '/'
+                                    ? 'Browse petitions'
+                                    : 'Submit petition'
+                        }
                     </Button>
                     <DropdownMenu>
                         <DropdownMenuTrigger>
@@ -96,10 +119,10 @@ const Navigation = observer(() => {
                         <DropdownMenuContent
                             align="end"
                             onCloseAutoFocus={(e) => e.preventDefault()}>
-                            <DropdownMenuItem onSelect={() => console.log('s')}>
+                            <DropdownMenuItem onSelect={() => console.log('Edit Profile')}>
                                 Edit Profile
                             </DropdownMenuItem>
-                            <DropdownMenuItem onSelect={() => console.log('s')}>
+                            <DropdownMenuItem onSelect={() => console.log('My Petitions')}>
                                 My Petitions
                             </DropdownMenuItem>
                             <DropdownMenuItem
