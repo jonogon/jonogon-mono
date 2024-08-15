20e588571225be1ac4594f859413a7a1116ff0c4
imas234 <sikdershadmansami@gmail.com>
2024-08-16 02:12:38 +0600

    Add image uploader and rearrange inputs in update petition

--- apps/jonogon-web/src/app/pages/updatePetition/index.tsx
+++ apps/jonogon-web/src/app/pages/updatePetition/index.tsx
@@ -1,16 +1,28 @@
 import Editor from '@/app/components/custom/richText';
 import {Button} from '@/app/components/ui/button';
+import {FileInput, FileUploader, FileUploaderContent, FileUploaderItem} from '@/app/components/ui/file-upload';
 import {Input} from '@/app/components/ui/input';
 import {Label} from '@/app/components/ui/label';
 import {useToast} from '@/app/components/ui/use-toast';
 import {trpc} from '@/app/trpc';
 import {useEffect, useState} from 'react';
+import {DropzoneOptions} from 'react-dropzone';
 import {useLocation, useParams} from 'wouter';
+
+const dropZoneOptions = {
+    accept: {
+        'image/*': ['.jpg', '.jpeg', '.png'],
+    },
+    multiple: true,
+    maxFiles: 4,
+    maxSize: 1 * 1024 * 1024,
+} as const satisfies DropzoneOptions;
 
 const UpdatePetition = () => {
     const {toast} = useToast();
     const {petition_id} = useParams();
-    const [, navigation] = useLocation();
+    const [_location, navigation] = useLocation();
+    const [files, setFiles] = useState<File[] | null>([]);
 
     const {mutateAsync: updatePetition} = trpc.petitions.update.useMutation();
     const {data: petitionRemoteData, isLoading: prdLoading} =
@@ -54,85 +66,90 @@ const UpdatePetition = () => {
         }
     };
 
+    if (prdLoading) {
+        return <div>loading</div>;
+    }
+
     return (
-        <div>
-            {prdLoading ? (
-                <>loading</>
-            ) : (
-                <div className="container">
-                    <h2 className="text-4xl mt-5 mb-4">
-                        Ensure all fields are filled with appropriate text for
-                        better reach and impact of your petition.
-                    </h2>
-                    <div className="flex flex-col gap-5 py-4">
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-                            <div className="flex flex-col gap-2">
-                                <Label htmlFor="target">
-                                    Enter the name of the ministry you wish to
-                                    target
-                                </Label>
-                                <Input
-                                    id="target"
-                                    value={petitionData.target}
-                                    onChange={(e) =>
-                                        handleUpdateData(
-                                            'target',
-                                            e.target.value,
-                                        )
-                                    }
-                                    placeholder="Ex- Ministry of Education"
-                                />
-                            </div>
-                            <div className="flex flex-col gap-2">
-                                <Label htmlFor="location">
-                                    Enter the location your petition is aimed at
-                                </Label>
-                                <Input
-                                    id="location"
-                                    value={petitionData.location}
-                                    onChange={(e) =>
-                                        handleUpdateData(
-                                            'location',
-                                            e.target.value,
-                                        )
-                                    }
-                                    placeholder="Ex- All over Bangladesh"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                            <Label htmlFor="title">Enter petition title</Label>
-                            <Input
-                                id="title"
-                                value={petitionData.title}
-                                onChange={(e) =>
-                                    handleUpdateData('title', e.target.value)
-                                }
-                                placeholder="Ex- Request for Enhanced Funding and Resources for Primary Education in Rural Areas"
-                            />
-                        </div>
-                        <div className="flex flex-col gap-2">
-                            <Label htmlFor="description">
-                                Describe the key issues and objectives of your
-                                petition
-                            </Label>
-                            <Editor
-                                content={petitionData.description}
-                                onChange={(val) =>
-                                    handleUpdateData('description', val)
-                                }
-                                placeholder="Write your post here..."
-                            />
-                        </div>
-                        <div className="flex items-center justify-center border border-neutral-400 text-neutral-600 rounded-md border-dashed h-28 select-none cursor-pointer">
-                            <div>Upload photos or videos</div>
-                        </div>
-                        <Button onClick={handleUpdatePetition}>
-                            Update petition
-                        </Button>
-                    </div>
+        <div className="flex flex-col gap-4 max-w-screen-sm mx-auto pt-5 pb-16 px-4">
+            <div className="flex flex-col gap-5 py-4">
+                {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+                    <Label htmlFor="location">
+                        Location
+                    </Label>
+                    <Input
+                        id="location"
+                        value={petitionData.location}
+                        onChange={(e) =>
+                            handleUpdateData(
+                                'location',
+                                e.target.value,
+                            )
+                        }
+                        placeholder="Ex- All over Bangladesh"
+                    />
+                </div> */}
+                <div className="flex flex-col gap-2">
+                    <Label htmlFor="title">Title</Label>
+                    <Input
+                        id="title"
+                        value={petitionData.title}
+                        onChange={(e) =>
+                            handleUpdateData('title', e.target.value)
+                        }
+                        placeholder="Ex: Request for Enhanced Funding and Resources for Primary Education in Rural Areas"
+                    />
                 </div>
-            )}
+                <div className="flex flex-col gap-2">
+                    <Label htmlFor="target">Target Ministry</Label>
+                    <Input
+                        id="target"
+                        value={petitionData.target}
+                        onChange={(e) =>
+                            handleUpdateData('target', e.target.value)
+                        }
+                        placeholder="Ex- Ministry of Education"
+                    />
+                </div>
+                <FileUploader
+                    value={files}
+                    onValueChange={setFiles}
+                    dropzoneOptions={dropZoneOptions}>
+                    <FileInput>
+                        <div className="flex items-center justify-center h-16 w-full border border-neutral-400 text-neutral-600 border-dashed bg-background rounded-md">
+                            <p className="text-gray-400">Upload Image</p>
+                        </div>
+                    </FileInput>
+                    <FileUploaderContent className="flex items-center flex-row gap-2">
+                        {files?.map((file, i) => (
+                            <FileUploaderItem
+                                key={i}
+                                index={i}
+                                className="size-20 p-0 rounded-md overflow-hidden"
+                                aria-roledescription={`file ${i + 1} containing ${file.name}`}>
+                                <img
+                                    src={URL.createObjectURL(file)}
+                                    alt={file.name}
+                                    height={80}
+                                    width={80}
+                                    className="size-20 p-0"
+                                />
+                            </FileUploaderItem>
+                        ))}
+                    </FileUploaderContent>
+                </FileUploader>
+                <div className="flex flex-col gap-2">
+                    <Label htmlFor="description">Details</Label>
+                    <Editor
+                        content={petitionData.description}
+                        onChange={(val) =>
+                            handleUpdateData('description', val)
+                        }
+                        placeholder="Write your post here..."
+                    />
+                </div>
+                <Button onClick={handleUpdatePetition}>Save</Button>
+            </div>
         </div>
     );
 };
