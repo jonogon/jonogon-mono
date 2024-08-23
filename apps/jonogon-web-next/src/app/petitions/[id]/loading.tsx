dd0226fea3ddf1216f83b264ee55985cce6ec627
Omran Jamal <o.jamal97@gmail.com>
2024-08-23 12:53:36 +0600

    petition loading screen

---
+++ apps/jonogon-web-next/src/app/petitions/[id]/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+        <div className={'pt-32 mx-auto max-w-screen-sm animate-pulse'}>
+            <div className={'bg-border w-1/2 h-7 my-4'}></div>
+            <div className={'bg-border w-full h-16 my-4'}></div>
+            <div className={'bg-border w-full h-96 my-4'}></div>
+        </div>
+    );
+}
