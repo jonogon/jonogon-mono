46d3e412d1233cef9c6ca3557fdce2b396c2d77c
Shadman Sakib <8457085+shadwan@users.noreply.github.com>
2024-09-07 20:59:58 +0600

    About modal and contributors page

--- apps/jonogon-web-next/src/components/custom/About.tsx
+++ apps/jonogon-web-next/src/components/custom/About.tsx
@@ -1,10 +1,45 @@
-import { ThumbsUp } from 'lucide-react';
+import { ThumbsUp, ChevronDown, ChevronUp } from 'lucide-react';
 import Image from 'next/image';
 import { Button } from '../ui/button';
 import { trpc } from '@/trpc/client';
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from '@/app/components/ui/collapsible';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
-export default function About() {
-	// Fixed array of contributors
+export default function About({ setOpen }: { setOpen: (open: boolean) => void }) {
+	const router = useRouter();
+	const [isOpen, setIsOpen] = useState(false);
+	const [isDesktop, setIsDesktop] = useState(false);
+
+	const handleModalClose = () => {
+		setOpen(false);
+		router.push('/contributors');
+	};
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsDesktop(window.innerWidth >= 768);
+		};
+
+		handleResize();
+		window.addEventListener('resize', handleResize);
+
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
+
+	useEffect(() => {
+		setIsOpen(isDesktop);
+	}, [isDesktop]);
+
+	const toggleCollapsible = () => {
+		setIsOpen(!isOpen);
+	};
+
 	const fixedContributors = [
 		'Hussain M Elius',
 		'Omran Jamal',
@@ -16,6 +51,13 @@ export default function About() {
 		'Tasnim Rahman',
 		'Syed Hasan',
 		'Ratul Karim',
+		'Tasnim Rahman',
+		'Syed Hasan',
+		'Ratul Karim',
+		'Ratul Karim',
+		'Tasnim Rahman',
+		'Syed Hasan',
+		'Ratul Karim',
 		'Hussain M Elius',
 		'Omran Jamal',
 		'Naseef Fatemi',
@@ -26,22 +68,30 @@ export default function About() {
 		'Tasnim Rahman',
 		'Syed Hasan',
 		'Ratul Karim',
+		'Tasnim Rahman',
+		'Syed Hasan',
+		'Ratul Karim',
+		'Ratul Karim',
+		'Tasnim Rahman',
+		'Syed Hasan',
+		'Ratul Karim',
 	];
 
-	// Fetch the list of contributors from tRPC
 	const { data: dynamicContributors = [], isLoading } =
 		trpc.users.getAllUserNames.useQuery();
 
-	// Combine fixed contributors and dynamic contributors, limiting the total to 20
 	const combinedContributors = [
 		...fixedContributors,
 		...dynamicContributors.map(
 			(contributor) => contributor.name ?? 'Anonymous',
 		),
-	].slice(0, 20); // Limit the list to 20 names
+	].slice(0, 20);
+
+	const isFixedContributor = (name: string) =>
+		fixedContributors.includes(name);
 
 	return (
-		<div className="flex flex-col">
+		<div className="flex flex-col overflow-y-auto md:overflow-y-visible">
 			<div className="flex flex-col md:flex-row justify-between mb-4">
 				<div className="w-full md:w-1/2 pr-4">
 					<div className="flex flex-row gap-8">
@@ -77,73 +127,100 @@ export default function About() {
 						</div>
 					</div>
 
-					<div className="mt-6 text-white space-y-4 font-light">
-						<p className="font-medium">
-							Jonogon is built for the people, by the people.
-						</p>
-						<p>
-							We consist of{' '}
-							<span className="font-medium">
-								2500+ citizens (and growing)
-							</span>{' '}
-							who believe in a secular and effective Bangladesh,
-							which stands for truth against violence, hatred,
-							crime, and discrimination.
-						</p>
-						<p>
-							A Bangladesh where everyone is free to speak without
-							fear.
-						</p>
-						<p>
-							A Bangladesh where reform takes place in days, not
-							decades.
-						</p>
-						<p>
-							A Bangladesh that works together to open doors of
-							opportunity for every citizen.
-						</p>
-						<p>
-							Contribute to this new Bangladesh by submitting your
-							দাবি today <br />{' '}
-							<span className="font-medium">
-								— we’ll ensure your voice matters and leads to
-								action.
-							</span>
-						</p>
+					<div className="mt-6 text-white font-light gap-4">
+						<Collapsible
+							open={isOpen}
+							onOpenChange={toggleCollapsible}
+							className="flex flex-col gap-4"
+						>
+							<CollapsibleTrigger className="flex justify-between items-center">
+								<p className="font-medium text-left">
+									Jonogon is built for the people, by the
+									people.
+								</p>
+								{/* Icon that changes based on whether the collapsible is open or closed */}
+								{isOpen ? (
+									<ChevronUp className="text-white w-5 h-5" />
+								) : (
+									<ChevronDown className="text-white w-5 h-5" />
+								)}
+							</CollapsibleTrigger>
+							<CollapsibleContent className="flex flex-col gap-4">
+								<p>
+									We consist of{' '}
+									<span className="font-medium">
+										2500+ citizens (and growing)
+									</span>{' '}
+									who believe in a secular and effective
+									Bangladesh, which stands for truth against
+									violence, hatred, crime, and discrimination.
+								</p>
+								<p>
+									A Bangladesh where everyone is free to speak
+									without fear.
+								</p>
+								<p>
+									A Bangladesh where reform takes place in
+									days, not decades.
+								</p>
+								<p>
+									A Bangladesh that works together to open
+									doors of opportunity for every citizen.
+								</p>
+								<p>
+									Contribute to this new Bangladesh by
+									submitting your দাবি today <br />{' '}
+									<span className="font-medium">
+										— we’ll ensure your voice matters and
+										leads to action.
+									</span>
+								</p>
+							</CollapsibleContent>
+						</Collapsible>
 					</div>
 				</div>
 
-				<div className="flex flex-col w-full md:w-1/2 md:border-l border-white border-opacity-40 md:pl-8 gap-4">
+				<div className="flex flex-col w-full md:w-1/2 md:border-l border-white border-opacity-20 pt-4 md:pt-0 md:pl-8 gap-4">
 					<p className="text-white font-medium text-xl">
-						Our Builders:
+						Our Contributors:
 					</p>
-					<ul className="list-disc list-inside columns-2 space-y-1 text-white font-light">
-						{/* Render the contributors' list */}
+					<ul className="list-disc list-inside md:columns-2 space-y-1 text-white font-light">
 						{isLoading ? (
 							<li>Loading...</li>
 						) : combinedContributors.length > 0 ? (
 							combinedContributors.map((contributor, index) => (
-								<li key={index}>{contributor}</li>
+								<li
+									key={index}
+									className="flex items-center gap-2"
+								>
+									{contributor}
+									{isFixedContributor(contributor) && (
+										<span>☆</span>
+									)}
+								</li>
 							))
 						) : (
 							<li>No contributors found</li>
 						)}
 					</ul>
 
-					{/* Show More button */}
 					{combinedContributors.length === 20 && (
-						<Button className="w-full bg-white text-black font-medium py-2 rounded-md mt-4">
-							Show More
+						<Button
+							variant="ghost"
+							className="text-white hover:bg-red-500"
+							onClick={() => handleModalClose()}
+						>
+							See All
 						</Button>
 					)}
 				</div>
 			</div>
 
-			<div className="flex flex-row gap-4">
-				<Button className="w-full bg-white text-white opacity-15 font-medium py-2 rounded-md">
+			<div className="flex flex-row gap-4 md:border-t border-white border-opacity-20 py-4">
+				<Button className="w-full bg-[#F7F2EE] bg-opacity-10 text-white font-medium py-4 rounded-md border-2 border-black border-opacity-10">
 					FAQS
 				</Button>
-				<Button className="w-full bg-white text-black font-medium py-2 rounded-md">
+				<Button className="w-full bg-[#F7F2EE] bg-opacity-10 text-white font-medium py-4 rounded-md border-2 border-black border-opacity-10">
 					Join our community
 				</Button>
 			</div>
