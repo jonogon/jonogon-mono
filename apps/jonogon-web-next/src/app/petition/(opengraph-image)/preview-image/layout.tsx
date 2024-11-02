b1f2092144532b70a341c9bb1fe3201a6e8c0bce
Ehsanur Rahman Rhythm <errhythm.me@gmail.com>
2024-11-03 01:02:54 +0600

    feat(ogimage): Created an API to get ogimage data and a template for frontend

---
+++ apps/jonogon-web-next/src/app/petition/(opengraph-image)/preview-image/layout.tsx
@@ -0,0 +1,38 @@
+export const metadata = {
+    title: 'Petition Preview',
+    robots: {
+        index: false,
+        follow: false,
+    },
+};
+
+export const viewport = {
+    width: '1200',
+    height: '630',
+    initialScale: 1,
+};
+
+export default function PreviewImageLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            <style
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    body {
+                        margin: 0;
+                        padding: 0;
+                        width: 1200px;
+                        height: 630px;
+                        overflow: hidden;
+                    }
+                `,
+                }}
+            />
+            {children}
+        </>
+    );
+}
